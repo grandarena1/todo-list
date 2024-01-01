c2a64f04a61ab964bfa346c82bec4e6cd94ab263
Travis Packer <travispacker1533@yahoo.co.uk>
2024-01-01 17:23:29 +0000

    Refactor form code to be reusable

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 import { Project } from "./project.js";
 
 //Containers
-const projectContainer = document.getElementsByClassName('projects-container')[0];
 let projects = [];
 
 //Buttons
 const addProjectButton = document.getElementsByClassName('add-project-btn-container')[0];
-addProjectButton.addEventListener('click', () => displayForm());
+addProjectButton.addEventListener('click', () => displayForm('project'));
 
 const confirmAddButton = document.getElementById('confirm-add-btn');
-confirmAddButton.addEventListener('click', () => createProject());
+confirmAddButton.addEventListener('click', () => createProject('project'));
 
 const cancelButton = document.getElementById('cancel-btn');
-cancelButton.addEventListener('click', () => hideForm());
+cancelButton.addEventListener('click', () => hideForm('project'));
+
+//Task form
+
+const addItemButton = document.getElementsByClassName('add-todo-item-btn')[0];
+addItemButton.addEventListener('click', () => displayForm('task'));
+
+const taskConfirmAddButton = document.getElementById('task-confirm-add-btn');
+taskConfirmAddButton.addEventListener('click', () => createProject('task'));
+
+const taskFormCancelButton = document.getElementById('task-cancel-btn');
+taskFormCancelButton.addEventListener('click', () => hideForm('task'));
 
 //Default project
 const defaultProjectContainer = new Project("Default Project", "Default Description");
@@ -24,17 +34,30 @@ const formContainer = document.getElementsByClassName('project-form')[0];
 const projectName = document.getElementsByClassName('form-title-input')[0];
 const projectDescription = document.getElementsByClassName('form-description-input')[0];
 
+const itemFormContainer = document.getElementsByClassName('item-form')[0];
+
 //Initialisation
 console.log(projects.length);
 defaultProjectContainer.changeProject();
 
 //Form functions
-function displayForm() {
-    formContainer.classList.remove('hidden');
+function displayForm(action) {
+    if(action === 'project') {
+        formContainer.classList.remove('hidden');
+    }
+    else if(action === 'task') {
+        itemFormContainer.classList.remove('hidden');
+    }
 }
 
-function hideForm() {
-    formContainer.classList.add('hidden');
+function hideForm(action) {
+    if(action === 'project') {
+        formContainer.classList.add('hidden');
+    }
+    else if(action === 'task') {
+        itemFormContainer.classList.add('hidden');
+    }
+
     resetForm();
 }
 
@@ -43,15 +66,20 @@ function resetForm() {
     projectDescription.value = '';
 }
 
-function createProject() {
+function createProject(action) {
     if(isValid()) {
-        const newProject = new Project(projectName.value, projectDescription.value);
-        newProject.setupUI();
-        projects.push(newProject);
-        newProject.changeProject();
-        console.log('Created new project: ' + newProject.name + ' / ' + newProject.description);
-        hideForm();
-        console.log('Current projects: ' + projects.length);
+        if(action === 'project') {
+            const newProject = new Project(projectName.value, projectDescription.value);
+            newProject.setupUI();
+            projects.push(newProject);
+            newProject.changeProject();
+            console.log('Created new project: ' + newProject.name + ' / ' + newProject.description);
+            hideForm('project');
+            console.log('Current projects: ' + projects.length);
+        }
+        else if(action === 'task') {
+
+        }
     }
     else {
         alert('please doob');
