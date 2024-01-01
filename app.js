1417d1b38f2e27e5f376c3f4e405c9f3a1cf07f3
Travis Packer <travispacker1533@yahoo.co.uk>
2024-01-01 02:44:43 +0000

    Add functionality to create a new project and display it on the UI

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-import { Project } from "./project";
+import { Project } from "./project.js";
 
 //Buttons
-
 const addProjectButton = document.getElementsByClassName('add-project-btn-container')[0];
 addProjectButton.addEventListener('click', () => displayForm());
 
+const confirmAddButton = document.getElementById('confirm-add-btn');
+confirmAddButton.addEventListener('click', () => createProject());
+
+const cancelButton = document.getElementById('cancel-btn');
+cancelButton.addEventListener('click', () => hideForm());
+
 //Form UI
+const formContainer = document.getElementsByClassName('project-form')[0];
+const projectName = document.getElementsByClassName('form-title-input')[0];
+const projectDescription = document.getElementsByClassName('form-description-input')[0];
 
-
+//Form functions
 function displayForm() {
-    
+    formContainer.classList.remove('hidden');
+}
+
+function hideForm() {
+    formContainer.classList.add('hidden');
+    resetForm();
+}
+
+function resetForm() {
+    projectName.value = '';
+    projectDescription.value = '';
+}
+
+function createProject() {
+    if(isValid()) {
+        const newProject = new Project(projectName.value, projectDescription.value);
+        newProject.setupUI();
+        console.log(newProject.name + ' ' + newProject.description);
+        hideForm();
+    }
+    else {
+        alert('please doob');
+    }
+}
+
+function isValid() {
+    if(projectName.value.length >= 1 && projectName.value.length <= 36 && projectDescription.value.length >= 1 && projectDescription.value.length <= 168) {
+        return true;
+    }
+
+    return false;
 }
--- project.js
+++ project.js
@@ -2,6 +2,7 @@ class Project {
     constructor(name, description) {
         this._name = name;
         this._description = description;
+        this._container = document.getElementsByClassName('projects-list-lower-left')[0];
     }
 
     get name() {
@@ -10,6 +11,29 @@ class Project {
 
     get description() {
         return this._description;
+    }
+    
+    get container() {
+        return this._container;
+    }
+
+    setupUI() {
+        //Container
+        const projectContainer = document.createElement('div');
+        projectContainer.classList.add('project');
+
+        //Project name
+        const projectName = document.createElement('h2');
+        projectName.textContent = this._name;
+        projectContainer.appendChild(projectName);
+
+        //Trash button
+        const projectRemoveButton = document.createElement('i');
+        projectRemoveButton.classList.add('fa-regular', 'fa-trash-can');
+        projectRemoveButton.style.color = '#ffffff';
+        projectContainer.appendChild(projectRemoveButton);
+
+        this._container.prepend(projectContainer);
     }
 }
 
